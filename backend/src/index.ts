07944d20afdcc837c01b1ba161b7e75d4d6026fa
Misha Marinenko <0x77dev@protonmail.com>
2020-05-26 19:01:37 +0300

    backend graphql setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,18 @@
 import "source-map-support/register";
+import { GraphQLServer } from 'graphql-yoga';
+import gql from "graphql-tag";
 
-console.log("Hello world!");
+const typeDefs = gql`
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_: void, { name }: { name: string }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log('Server is running on http://localhost:4000'));
